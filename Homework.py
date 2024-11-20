be1de8c77e7ad98b01dc6af3aad927e57be96af5
Makoto <dimonkrupko@yandex.ru>
2024-11-20 17:27:54 +0300

    add\whithout_last

--- Homework.py
+++ Homework.py
@@ -4,3 +4,5 @@ print(text.upper())
 #print(text)
 without_a = text.replace('А', '').replace('а', '').replace('A', '').replace('a', '')
 print(without_a)
+text_whithout_last = text[:-1]
+print(text_whithout_last)
